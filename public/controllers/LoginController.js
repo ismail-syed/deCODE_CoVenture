461b29ca76f836e4c90f5e1c427c9f1dc097b104
James-Thibaudeau <jamesthibaudeau@gmail.com>
2016-10-14 19:55:04 -0400

    fixed git issue

--- public/controllers/LoginController.js
+++ public/controllers/LoginController.js
@@ -4,8 +4,15 @@ app.controller("LoginController", function($scope, $location) {
 
     console.log("LoginController");
 
-    $scope.changeView = function ($scope) {
-      $location.path('/dashboard')
+    $scope.companies = [
+      'Amazon',
+      'Klipfolio',
+      'CoVenture',
+      'You.i'
+    ]
+
+    $scope.getCompany = function($scope) {
+      
     }
 
 });
