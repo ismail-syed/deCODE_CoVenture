91f077bbdb2aa7f917b219b43c11eb98252a5847
= <isyed867@gmail.com>
2016-10-14 15:11:11 -0400

    Added files

---
+++ dashboard-api/models/Company.js
@@ -0,0 +1,23 @@
+// grab the things we need
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var Schema = mongoose.Schema;
+
+// create a schema
+var CompanySchema = new Schema({
+  name: String,
+  created_at: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  updated_at: Date
+});
+
+
+// the schema is useless so far
+// we need to create a model using it
+var Company = mongoose.model('Company', CompanySchema);
+
+// make this available to our users in our Node applications
+module.exports = Company;
