284490f4d0e3697582464ae8a5e283fed59ae136
xycong <xycong@uwaterloo.ca>
2016-10-14 12:38:47 -0400

    Connecting routes with models

--- dashboard-api/server.js
+++ dashboard-api/server.js
@@ -4,8 +4,6 @@ var app = express();
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
-var http = require('http');
-var server = http.createServer(app);
 // var mongoose = require('mongoose');
 // var mongoURL = "mongodb://node:node@ds023644.mlab.com:23644/dashboard-api";
 // mongoose.connect(mongoURL);
@@ -17,20 +15,11 @@ app.use(cors());
 
 var port = process.env.PORT || 8080;        // set our port
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
-
 // more routes for our API will happen here
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-app.use('/api', router);
+app.use('/user', require('./controllers/user'));
 
 // START THE SERVER
 // =============================================================================
