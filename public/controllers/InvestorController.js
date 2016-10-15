2dbd6e37f17161b9a2d2e33ef25120c40e342cb0
Grant Zeidler <Grant Zeidler>
2016-10-15 10:18:33 -0400

    First attempt at InvestorController.js backend

--- public/controllers/InvestorController.js
+++ public/controllers/InvestorController.js
@@ -1,7 +1,36 @@
 var app = angular.module('CoVentureApp');
 
-app.controller("InvestorController", function($scope, $location) {
+app.controller("InvestorController", '$scope', '$location', 'GoalService', function($scope, $location, GoalService) {
+    $scope.loadGoals = function() {
+      GoalService.getGoal(selectedCompanyId).then(function(goals) {
+        $scope.questions = [];
+        for(goal in goals) {
+          $scope.questions.push({"title": goal["label"], "name": goal["action"], "freq": goal["occurrence"], "repr": "GRAPH"});
+        }
+      }).catch(function() {
+        $scope.error = 'Unable to get the goals';
+      });
+    }
 
     console.log("InvestorController");
+    var selectedCompanyId = null;
 
+    $scope.frequencies = [
+      "WEEKLY",
+      "MONTHLY",
+      "QUARTERLY",
+      "YEARLY"
+    ];
+
+    $scope.representations = [
+      "GRAPH",
+      "TABLE"
+    ];
+
+    $scope.submit = function() {
+      var goal = {"variableAction": this.createName, "variableLabel": this.createTitle, "companyId": selectedCompanyId};
+      // TODO add occurrence and repr to goal (this.createFreq, this.createRepr)
+      GoalService.createGoal(goal, selectedCompanyId);
+      $scope.questions.push({"title": this.createTitle, "name": this.createName, "freq": this.createFreq, "repr": this.createRepr});
+    }
 });
