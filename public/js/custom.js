177836a924e311109c15a68be65baddd01008a57
Vincent <vincentt117@gmail.com>
2016-10-15 17:04:32 -0400

    Added charts

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,18 +1,8 @@
-$(document).ready(function() {
-    
-    $('.investor-login-link').on('click',function(e) {
-		$("#investor-login").delay(100).fadeIn(100);
- 		$("#company-login").fadeOut(100);
-		$('#company-login-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-	$('.company-login-link').on('click',function(e) {
-		$("#company-login").delay(100).fadeIn(100);
- 		$("#investor-login").fadeOut(100);
-		$('#investor-login-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-
-});
+window.showGraph = function(){
+    var doc = document.getElementById("chartdiv");
+    doc.className = "shown";
+}
+window.hideGraph = function(){
+    var doc = document.getElementById("chartdiv");
+    doc.className = "hidden";
+}
