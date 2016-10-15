4ee314a9f0d9181217e1549bd6283fbd0c781a77
xycong <xycong@uwaterloo.ca>
2016-10-14 21:54:27 -0400

    Fixed service

--- public/controllers/LoginController.js
+++ public/controllers/LoginController.js
@@ -1,8 +1,7 @@
 var app = angular.module('CoVentureApp');
 
 app.controller("LoginController", ['$scope', 'CompanyService', function($scope, CompanyService) {
-    var companyService = new CompanyService();
-    companyService.getCompanies().then(function(data) {
+    CompanyService.getCompanies().then(function(data) {
       $scope.companies = data;
     }).catch(function() {
       $scope.error = 'Unable to get the companies';
